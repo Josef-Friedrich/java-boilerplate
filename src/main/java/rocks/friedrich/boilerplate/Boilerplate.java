252c476a242822a74ade21c3f0c62f2febe589be
Josef Friedrich <josef@friedrich.rocks>
2023-11-20 20:46:37 +0100

    Add docs

--- src/main/java/rocks/friedrich/boilerplate/Boilerplate.java
+++ src/main/java/rocks/friedrich/boilerplate/Boilerplate.java
@@ -1,7 +1,15 @@
 package rocks.friedrich.boilerplate;
 
+/**
+ * A boilerplate class.
+ */
 public class Boilerplate {
 
+  /**
+   * Return the string “Hello, World!”.
+   *
+   * @return The string “Hello, World!”.
+   */
   public static String getHelloWorld() {
     return "Hello, World!";
   }
